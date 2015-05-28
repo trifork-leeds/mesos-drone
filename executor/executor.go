0e2d333b4973d57b6419029cb1e60c186a1e93da
Phil Winder <pnw@trifork.com>
2015-05-28 11:38:30 +0100

    Changed sout messages to glog

--- executor/executor.go
+++ executor/executor.go
@@ -3,9 +3,9 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	exec "github.com/mesos/mesos-go/executor"
 	mesos "github.com/mesos/mesos-go/mesosproto"
+	log "github.com/golang/glog"
 )
 
 type exampleExecutor struct {
@@ -17,19 +17,19 @@ func newExampleExecutor() *exampleExecutor {
 }
 
 func (exec *exampleExecutor) Registered(driver exec.ExecutorDriver, execInfo *mesos.ExecutorInfo, fwinfo *mesos.FrameworkInfo, slaveInfo *mesos.SlaveInfo) {
-	fmt.Println("Registered Executor on slave ", slaveInfo.GetHostname())
+	log.Info("Registered Executor on slave ", slaveInfo.GetHostname())
 }
 
 func (exec *exampleExecutor) Reregistered(driver exec.ExecutorDriver, slaveInfo *mesos.SlaveInfo) {
-	fmt.Println("Re-registered Executor on slave ", slaveInfo.GetHostname())
+	log.Info("Re-registered Executor on slave ", slaveInfo.GetHostname())
 }
 
 func (exec *exampleExecutor) Disconnected(exec.ExecutorDriver) {
-	fmt.Println("Executor disconnected.")
+	log.Info("Executor disconnected.")
 }
 
 func (exec *exampleExecutor) LaunchTask(driver exec.ExecutorDriver, taskInfo *mesos.TaskInfo) {
-	fmt.Println("Launching task", taskInfo.GetName(), "with command", taskInfo.Command.GetValue())
+	log.Info("Launching task", taskInfo.GetName(), "with command", taskInfo.Command.GetValue())
 
 	runStatus := &mesos.TaskStatus{
 		TaskId: taskInfo.GetTaskId(),
@@ -37,42 +37,42 @@ func (exec *exampleExecutor) LaunchTask(driver exec.ExecutorDriver, taskInfo *me
 	}
 	_, err := driver.SendStatusUpdate(runStatus)
 	if err != nil {
-		fmt.Println("Got error", err)
+		log.Error("Got error", err)
 	}
 
 	exec.tasksLaunched++
-	fmt.Println("Total tasks launched ", exec.tasksLaunched)
+	log.Info("Total tasks launched ", exec.tasksLaunched)
 	//
 	// this is where one would perform the requested task
 	//
 
 	// finish task
-	fmt.Println("Finishing task", taskInfo.GetName())
+	log.Info("Finishing task", taskInfo.GetName())
 	finStatus := &mesos.TaskStatus{
 		TaskId: taskInfo.GetTaskId(),
 		State:  mesos.TaskState_TASK_FINISHED.Enum(),
 	}
 	_, err = driver.SendStatusUpdate(finStatus)
 	if err != nil {
-		fmt.Println("Got error", err)
+		log.Error("Got error", err)
 	}
-	fmt.Println("Task finished", taskInfo.GetName())
+	log.Info("Task finished", taskInfo.GetName())
 }
 
 func (exec *exampleExecutor) KillTask(exec.ExecutorDriver, *mesos.TaskID) {
-	fmt.Println("Kill task")
+	log.Info("Kill task")
 }
 
 func (exec *exampleExecutor) FrameworkMessage(driver exec.ExecutorDriver, msg string) {
-	fmt.Println("Got framework message: ", msg)
+	log.Info("Got framework message: ", msg)
 }
 
 func (exec *exampleExecutor) Shutdown(exec.ExecutorDriver) {
-	fmt.Println("Shutting down the executor")
+	log.Info("Shutting down the executor")
 }
 
 func (exec *exampleExecutor) Error(driver exec.ExecutorDriver, err string) {
-	fmt.Println("Got error message:", err)
+	log.Error("Got error message:", err)
 }
 
 // -------------------------- func inits () ----------------- //
@@ -81,7 +81,7 @@ func init() {
 }
 
 func main() {
-	fmt.Println("Starting Example Executor (Go)")
+	log.Info("Starting Example Executor (Go)")
 
 	dconfig := exec.DriverConfig{
 		Executor: newExampleExecutor(),
@@ -89,14 +89,14 @@ func main() {
 	driver, err := exec.NewMesosExecutorDriver(dconfig)
 
 	if err != nil {
-		fmt.Println("Unable to create a ExecutorDriver ", err.Error())
+		log.Warning("Unable to create a ExecutorDriver ", err.Error())
 	}
 
 	_, err = driver.Start()
 	if err != nil {
-		fmt.Println("Got error:", err)
+		log.Error("Got error:", err)
 		return
 	}
-	fmt.Println("Executor process has started and running.")
+	log.Info("Executor process has started and running.")
 	driver.Join()
 }

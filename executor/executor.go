7399f87361a041e3c4fb6ec7eca87ed5d1204cdb
Bamo Nadir <bamonadir@BAMO.local>
2015-05-29 14:47:49 +0100

    changed port to 80

--- executor/executor.go
+++ executor/executor.go
@@ -5,10 +5,9 @@ import (
 	"flag"
 	exec "github.com/mesos/mesos-go/executor"
 	mesos "github.com/mesos/mesos-go/mesosproto"
-	"fmt"
+	log "github.com/golang/glog"
 	exe "os/exec"
 	"os"
-	"strings"
 )
 
 type exampleExecutor struct {
@@ -20,27 +19,19 @@ func newExampleExecutor() *exampleExecutor {
 }
 
 func (exec *exampleExecutor) Registered(driver exec.ExecutorDriver, execInfo *mesos.ExecutorInfo, fwinfo *mesos.FrameworkInfo, slaveInfo *mesos.SlaveInfo) {
-	fmt.Println("Registered Executor on slave ", slaveInfo.GetHostname())
+	log.Info("Registered Executor on slave ", slaveInfo.GetHostname())
 }
 
 func (exec *exampleExecutor) Reregistered(driver exec.ExecutorDriver, slaveInfo *mesos.SlaveInfo) {
-	fmt.Println("Re-registered Executor on slave ", slaveInfo.GetHostname())
+	log.Info("Re-registered Executor on slave ", slaveInfo.GetHostname())
 }
 
 func (exec *exampleExecutor) Disconnected(exec.ExecutorDriver) {
-	fmt.Println("Executor disconnected.")
+	log.Info("Executor disconnected.")
 }
 
 func (exec *exampleExecutor) LaunchTask(driver exec.ExecutorDriver, taskInfo *mesos.TaskInfo) {
-	fmt.Println("Launching task", taskInfo.GetName(), "with command", taskInfo.Command.GetValue())
-	data := string(taskInfo.Data)
-	fmt.Println("Custom data: ", data)
-
-	// TODO: This is a really crappy way of passing arguments!
-	commands := strings.Split(data, " ")
-
-	// TODO: This will crash if there are not two arguments!
-	fmt.Println("Commands: ", commands[0], commands[1])
+	log.Info("Launching task", taskInfo.GetName(), "with command", taskInfo.Command.GetValue())
 
 	runStatus := &mesos.TaskStatus{
 		TaskId: taskInfo.GetTaskId(),
@@ -48,49 +39,49 @@ func (exec *exampleExecutor) LaunchTask(driver exec.ExecutorDriver, taskInfo *me
 	}
 	_, err := driver.SendStatusUpdate(runStatus)
 	if err != nil {
-		fmt.Println("Got error", err)
+		log.Error("Got error", err)
 	}
 
 	exec.tasksLaunched++
-	fmt.Println("Total tasks launched ", exec.tasksLaunched)
+	log.Info("Total tasks launched ", exec.tasksLaunched)
 
-	fmt.Println("Executing drone-agent")
-	droneCmd := exe.Command("drone-agent",commands[0], commands[1])
+	log.Info("Executing drone-agent")
+	droneCmd := exe.Command("drone-agent","-addr=http://localhost:8000","-token=1")
 	droneCmd.Stdout = os.Stdout
 	droneCmd.Stderr = os.Stderr
 	err = droneCmd.Run()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Completed drone-agent")
+	log.Info("Completed drone-agent")
 
 	// finish task
-	fmt.Println("Finishing task", taskInfo.GetName())
+	log.Info("Finishing task", taskInfo.GetName())
 	finStatus := &mesos.TaskStatus{
 		TaskId: taskInfo.GetTaskId(),
 		State:  mesos.TaskState_TASK_FINISHED.Enum(),
 	}
 	_, err = driver.SendStatusUpdate(finStatus)
 	if err != nil {
-		fmt.Println("Got error", err)
+		log.Error("Got error", err)
 	}
-	fmt.Println("Task finished", taskInfo.GetName())
+	log.Info("Task finished", taskInfo.GetName())
 }
 
 func (exec *exampleExecutor) KillTask(exec.ExecutorDriver, *mesos.TaskID) {
-	fmt.Println("Kill task")
+	log.Info("Kill task")
 }
 
 func (exec *exampleExecutor) FrameworkMessage(driver exec.ExecutorDriver, msg string) {
-	fmt.Println("Got framework message: ", msg)
+	log.Info("Got framework message: ", msg)
 }
 
 func (exec *exampleExecutor) Shutdown(exec.ExecutorDriver) {
-	fmt.Println("Shutting down the executor")
+	log.Info("Shutting down the executor")
 }
 
 func (exec *exampleExecutor) Error(driver exec.ExecutorDriver, err string) {
-	fmt.Println("Got error message:", err)
+	log.Error("Got error message:", err)
 }
 
 // -------------------------- func inits () ----------------- //
@@ -99,8 +90,7 @@ func init() {
 }
 
 func main() {
-
-	fmt.Println("Starting Example Executor (Go)")
+	log.Info("Starting Example Executor (Go)")
 
 	dconfig := exec.DriverConfig{
 		Executor: newExampleExecutor(),
@@ -108,14 +98,14 @@ func main() {
 	driver, err := exec.NewMesosExecutorDriver(dconfig)
 
 	if err != nil {
-		fmt.Println("Unable to create a ExecutorDriver ", err.Error())
+		log.Warning("Unable to create a ExecutorDriver ", err.Error())
 	}
 
 	_, err = driver.Start()
 	if err != nil {
-		fmt.Println("Got error:", err)
+		log.Error("Got error:", err)
 		return
 	}
-	fmt.Println("Executor process has started and running.")
+	log.Info("Executor process has started and running.")
 	driver.Join()
 }
